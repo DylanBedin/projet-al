a19df02d7f83428e2a7a79bc0052961a0a2f0cb6
Gael Trottier <gaetrottier@emi.u-bordeaux1.fr>
2017-03-27 09:03:14 +0200

    DragAndDrop dans view

--- src/view/View.java
+++ src/view/View.java
@@ -1,20 +1,63 @@
 package view;
 
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import model.*;
 
 public class View extends Application{
 
 	public static void main(String[] args) {
         Application.launch(View.class, args);
     }
+	
+	 /***********************************************************************************/
+    double orgSceneX, orgSceneY;
+    double orgTranslateX, orgTranslateY;
+    /***********************************************************************************/
+    EventHandler<MouseEvent> rectOnMousePressedEventHandler =
+    new EventHandler<MouseEvent>() {
+
+    @Override
+    public void handle(MouseEvent t) {
+    orgSceneX = t.getSceneX();
+    orgSceneY = t.getSceneY();
+    orgTranslateX = ((Rectangle)(t.getSource())).getTranslateX();
+    orgTranslateY = ((Rectangle)(t.getSource())).getTranslateY();
+    }
+    };
+
+    EventHandler<MouseEvent> rectOnMouseDraggedEventHandler =
+    new EventHandler<MouseEvent>() {
+
+    @Override
+    public void handle(MouseEvent t) {
+    double offsetX = t.getSceneX() - orgSceneX;
+    double offsetY = t.getSceneY() - orgSceneY;
+    double newTranslateX = orgTranslateX + offsetX;
+    double newTranslateY = orgTranslateY + offsetY;
+
+    ((Rectangle)(t.getSource())).setTranslateX(newTranslateX);
+    ((Rectangle)(t.getSource())).setTranslateY(newTranslateY);
+    }
+    };
+    /*******************************************************************************/
+	
+	
+	
+	
+	
 	
 	@Override
     public void start(Stage primaryStage) {
@@ -32,6 +75,17 @@ public class View extends Application{
         gr1.setLayoutX(5);
         gr1.setLayoutY(5);
         
+        Rectangle rect1 = new Rectangle();
+        rect1.setX(5);
+        rect1.setY(5);
+        rect1.setWidth(480);
+        rect1.setHeight(50);
+        rect1.setArcWidth(30);
+        rect1.setArcHeight(30);
+        rect1.setFill(Color.WHITE);
+        rect1.setStroke(Color.BLACK);
+        rect1.setMouseTransparent(true);
+        
         Button btnSave = new Button();
         btnSave.setLayoutX(15);
         btnSave.setLayoutY(15);
@@ -47,25 +101,25 @@ public class View extends Application{
         Button btnUndo = new Button();
         btnUndo.setLayoutX(145);
         btnUndo.setLayoutY(15);
-        btnUndo.setText("Undo");
+        Image UndoImg = new Image(getClass().getResourceAsStream("../img/Undo.png"));
+        ImageView uI = new ImageView(UndoImg);
+        uI.setFitHeight(20);
+        uI.setFitWidth(20);
+        btnUndo.setGraphic(uI);
+        //btnUndo.setText("Undo");
         //root.getChildren().add(btnUndo);
         
         Button btnRedo = new Button();
         btnRedo.setLayoutX(200);
         btnRedo.setLayoutY(15);
-        btnRedo.setText("Redo");
+        Image RedoImg = new Image(getClass().getResourceAsStream("../img/Redo.png"));
+        ImageView rI = new ImageView(RedoImg);
+        rI.setFitHeight(20);
+        rI.setFitWidth(20);
+        btnRedo.setGraphic(rI);
+        //btnRedo.setText("Redo");
         //root.getChildren().add(btnRedo);
-        
-        Rectangle rect1 = new Rectangle();
-        rect1.setX(5);
-        rect1.setY(5);
-        rect1.setWidth(480);
-        rect1.setHeight(50);
-        rect1.setArcWidth(30);
-        rect1.setArcHeight(30);
-        rect1.setFill(Color.WHITE);
-        rect1.setStroke(Color.BLACK);
-        
+
         
         gr1.getChildren().add(rect1);
         gr1.getChildren().add(btnRedo);
@@ -83,13 +137,6 @@ public class View extends Application{
         gr2.setLayoutX(5);
         gr2.setLayoutY(50);
         
-        //Liste des formes 
-        
-        Button TrashCan = new Button();
-        TrashCan.setLayoutX(7);
-        TrashCan.setLayoutY(490);
-        TrashCan.setText("TrashCan");
-        
         Rectangle rect2 = new Rectangle();
         rect2.setX(5);
         rect2.setY(20);
@@ -99,10 +146,26 @@ public class View extends Application{
         rect2.setArcHeight(30);
         rect2.setFill(Color.WHITE);
         rect2.setStroke(Color.BLACK);
+        rect2.setMouseTransparent(true);
+        
+        
+        //Liste des formes 
+        
+        Button TrashCan = new Button();
+        TrashCan.setLayoutX(20);
+        TrashCan.setLayoutY(490);
+        Image TrashCanImg = new Image(getClass().getResourceAsStream("../img/TrashCan.png"));
+        ImageView iV = new ImageView(TrashCanImg);
+        iV.setFitHeight(20);
+        iV.setFitWidth(20);
+        TrashCan.setGraphic(iV);
+        //TrashCan.setText("TrashCan");
+        
         
         gr2.getChildren().add(rect2);
         gr2.getChildren().add(TrashCan);
         root.getChildren().add(gr2);
+        
 /***********************************************************************************************/
         /*
          * TROISIEME GROUPE: Fenetre contenant les formes à afficher.
@@ -142,6 +205,24 @@ public class View extends Application{
         gr3.getChildren().add(sc1);
         gr3.getChildren().add(sc2);
         root.getChildren().add(gr3);
+        
+
+        /*********AJOUT DU RECTANGLE D'ESSAIE********/
+        Rectangle rectTest = new Rectangle();
+        rectTest.setX(200);
+        rectTest.setY(200);
+        rectTest.setWidth(200);
+        rectTest.setHeight(200);
+        rectTest.setFill(Color.BLACK);
+        rectTest.setStroke(Color.BLACK);
+        rectTest.setOnMousePressed(rectOnMousePressedEventHandler);
+        rectTest.setOnMouseDragged(rectOnMouseDraggedEventHandler);
+        root.getChildren().add(rectTest);
+
+
+       
+        
+        
         
         primaryStage.setScene(scene);
         primaryStage.show();
